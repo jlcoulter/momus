8dff39ced2a16b07252bb22a5de022c0590ad563
jlcoulter <coulter.l.jack@gmail.com>
2026-08-18 15:12:05 +1000

    feat: add richer fhir metadata structures

--- internal/fhir/model/valueset.go
+++ internal/fhir/model/valueset.go
@@ -2,9 +2,27 @@ package model
 
 // ValueSet is the minimal normalised representation of a FHIR ValueSet.
 type ValueSet struct {
-	URL     string
-	Version string
-	Name    string
-	Status  string
-	// Expansion and composition are intentionally omitted for now.
+	URL               string
+	Version           string
+	Name              string
+	Status            string
+	ComposeIncludes   []ValueSetInclude
+	ExpansionContains []ValueSetExpansionContains
+}
+
+type ValueSetInclude struct {
+	System   string
+	Concepts []ConceptReference
+}
+
+type ValueSetExpansionContains struct {
+	System   string
+	Code     string
+	Display  string
+	Contains []ValueSetExpansionContains
+}
+
+type ConceptReference struct {
+	Code    string
+	Display string
 }
